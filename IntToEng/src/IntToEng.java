abadb9377353730ec71a6e111aeea6f4dcdef727
g11909hm <g11909hm+git@gm.tsuda.ac.jp>
2014-05-12 13:54:10 +0900

    20までの数字を入力すると英語に変えるプログラムを作った

--- IntToEng/src/IntToEng.java
+++ IntToEng/src/IntToEng.java
@@ -4,13 +4,18 @@ public class IntToEng {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int input = sc.nextInt();
-        
         System.out.println(translateEng(input));
-        
     }
     
     // 数値を英訳する変換するメソッド
     static String translateEng(int n) {
-        return "";
+    	int rest=n;
+        return tlTwenty(rest);
+    }
+    
+    static String tlTwenty(int r){
+    	String a[]=new String[]{"zero","one","two","three","four","five","six","seven","eight","nine","ten",
+    			"eleven","twelve","thirteen","fourtenn","fifteen","sixteen","seventeen","eighteen","nineteen","twenty"};	
+    	return a[r];
     }
 }
